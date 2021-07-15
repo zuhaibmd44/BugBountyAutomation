c414bdd7b11cb0f5380cca9945d573c230125ffb
root <zuhaibmd44@gmail.com>
2021-07-15 11:53:34 -0400

    removed unwanted comments and hardcoded data

--- getGitDorkurls.sh
+++ getGitDorkurls.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 #Zuhaib Mohammed - July 2021
-#This script is get all the urls with the required organization and git dorks
-#Command to run -> ./getGitDorkurls.sh organisation git_worldlist 
+#This script is get all the urls with the organization and git dorks
+#Command to run -> ./getGitDorkurls.sh <organisation> <git_worldlist> 
 #Install Open Multiple URLs on Chrome to open around 25 tabs at once
-domain="Unity-Technologies"
-wordlist="sampleDorks.txt"
-
-#url example -> https://github.com/search?o=desc&q=org:Unity-Technologies+token&s=indexed&type=Code
+org=$1
+wordlist=$2
 
 while IFS= read -r line
 do
-    echo "https://github.com/search?q=org:$domain+'$line'&s=indexed&type=Code"
+    echo "https://github.com/search?q=org:$org+'$line'&s=indexed&type=Code"
 
 done < "$wordlist"
